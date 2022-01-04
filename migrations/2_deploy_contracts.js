fa7682c09a1c4ffc29d280b36a39f32fbfbec118
Jay <mtroz310@gmail.com>
2022-01-04 22:12:00 +0800

    first commit

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,21 @@
+const DAO = artifacts.require("MultiSigWalletWithDailyLimit");
+const MemoriesERC20 = artifacts.require("MemoriesERC20");
+const TimeERC20 = artifacts.require("TimeERC20");
+const Staking = artifacts.require("Staking");
+const StakingHelper = artifacts.require("StakingHelper");
+const StandardBondingCalculator = artifacts.require("StandardBondingCalculator");
+const Treasury = artifacts.require("Treasury");
+const WonderZapIn = artifacts.require("WonderZapIn");
+const wMEMO = artifacts.require("wMEMO");
+
+module.exports = function (deployer) {
+  const daoAddress = deployer.deploy(DAO);
+  // const memoriesERC20 = deployer.deploy(MemoriesERC20);
+  // const timeERC20Token = deployer.deploy(TimeERC20);
+  // const stakingAddress = deployer.deploy(Staking, timeERC20Token.address, memoriesERC20.address, 28800, 440, 1641294975);
+  // const stakingHelper = deployer.deploy(StakingHelper, staking.address, timeERC20TokenAddress.address);
+  // const standardBondingCalculator = deployer.deploy(StandardBondingCalculator, timeERC20TokenAddress.address);
+  // const treasury = deployer.deploy(Treasury);
+  // const wonderZapIn = deployer.deploy(WonderZapIn, daoAddress.address);
+  // const wMEMO_ = deployer.deploy(wMEMO, memoriesERC20.address);
+};
