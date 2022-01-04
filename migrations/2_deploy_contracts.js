88342ba6144ae5a6df5f0e656059fa489843ba87
davidm <ok.star@outlook.com>
2022-01-05 05:51:23 +0800

    deploy timeERC20

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,4 @@
-const DAO = artifacts.require("MultiSigWalletWithDailyLimit");
-// const MemoriesERC20 = artifacts.require("MemoriesERC20");
-// const TimeERC20 = artifacts.require("TimeERC20");
+const TimeERC20 = artifacts.require("TimeERC20Token");
 // const Staking = artifacts.require("Staking");
 // const StakingHelper = artifacts.require("StakingHelper");
 // const StandardBondingCalculator = artifacts.require("StandardBondingCalculator");
@@ -9,9 +7,7 @@ const DAO = artifacts.require("MultiSigWalletWithDailyLimit");
 // const wMEMO = artifacts.require("wMEMO");
 
 module.exports = function (deployer) {
-  const daoAddress = deployer.deploy(DAO);
-  // const memoriesERC20 = deployer.deploy(MemoriesERC20);
-  // const timeERC20Token = deployer.deploy(TimeERC20);
+  const timeERC20Token = deployer.deploy(TimeERC20);
   // const stakingAddress = deployer.deploy(Staking, timeERC20Token.address, memoriesERC20.address, 28800, 440, 1641294975);
   // const stakingHelper = deployer.deploy(StakingHelper, staking.address, timeERC20TokenAddress.address);
   // const standardBondingCalculator = deployer.deploy(StandardBondingCalculator, timeERC20TokenAddress.address);
