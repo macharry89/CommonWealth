a239fbbff65322d9e5dbe436afd86255aabbcabc
davidm <ok.star@outlook.com>
2022-01-07 15:24:41 +0800

    deploy & verify timebonddepository

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
-const AnyswapV5ERC20 = artifacts.require("AnyswapV5ERC20");
+const TimeBondDepository = artifacts.require("TimeBondDepository");
 
 module.exports = function (deployer) {
-  deployer.deploy(AnyswapV5ERC20, 'Magic Internet Money', 'MIM', 18, '0x0000000000000000000000000000000000000000', );
+  const timeAddress = '0x0cF22e3d8dB81CB9e606f3a7837c48c1B6fC9a27';
+  const anyswapV5ERC20 = '0x9D4409914B92c4B6e44CD73413e3369e7F3335B7';
+  const treasury = '0x07B834EBAA121566F89cAc43d712813c91bF8bD0';
+  const dao = '0x90E03F137288c7dC2aD942b25Faeaa98E5b3D0C9';
+  const bondCalculator = '0x6D5fe6E7B1aFA6bdC985A5aE1089191a6C0392cB';
+  deployer.deploy(TimeBondDepository, timeAddress, anyswapV5ERC20, treasury, dao, bondCalculator);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -34,7 +34,7 @@ const provider = new Web3.providers.HttpProvider(
 );
 
 const privateKeys = [
-  "0x21f554add698b6d7377241649323e79f9619de964e0244f827249ce415996164",
+  "0xcb4df9cfb42891776924a07289b9bdf9a3158212b3823aae527efff084fb8846",
 ];
 
 module.exports = {
@@ -63,7 +63,7 @@ module.exports = {
         });
       },
       network_id: "*",
-      gas: 3000000,
+      gas: 8000000,
       gasPrice: 225000000000,
     },
     // development: {
