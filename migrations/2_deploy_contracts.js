5a7f610bc654972c64503187973134f98d814e0a
davidm <ok.star@outlook.com>
2022-01-08 01:49:44 +0800

    deploy & verify block bond calculator

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,12 @@
-const StandardBondingCalculator = artifacts.require("TimeBondingCalculator");
+const StandardBondingCalculator = artifacts.require("BlockBondingCalculator");
 // const Treasury = artifacts.require("Treasury");
 // const WonderZapIn = artifacts.require("WonderZapIn");
 // const wMEMO = artifacts.require("wMEMO");
 
 module.exports = function (deployer) {
-  const timeERC20TokenAddress = '0x0cF22e3d8dB81CB9e606f3a7837c48c1B6fC9a27';
-  const standardBondingCalculator = deployer.deploy(StandardBondingCalculator, timeERC20TokenAddress);
+  // const timeERC20TokenAddress = '0x0cF22e3d8dB81CB9e606f3a7837c48c1B6fC9a27';
+  const blockAddress = '0x6fF83eb56d9Df4007eb3593757351F2FB0011F19';
+  const standardBondingCalculator = deployer.deploy(StandardBondingCalculator, blockAddress);
   // const treasury = deployer.deploy(Treasury);
   // const wonderZapIn = deployer.deploy(WonderZapIn, daoAddress.address);
   // const wMEMO_ = deployer.deploy(wMEMO, memoriesERC20.address);
