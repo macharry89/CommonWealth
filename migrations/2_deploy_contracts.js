770a2ee600af114cec48422eceb9969286c1a928
davidm <ok.star@outlook.com>
2022-01-08 01:58:19 +0800

    deploy & verify block treasury

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,9 @@
-const Treasury = artifacts.require("TimeTreasury");
-// const WonderZapIn = artifacts.require("WonderZapIn");
-// const wMEMO = artifacts.require("wMEMO");
+const Treasury = artifacts.require("BlockTreasury");
 
 module.exports = function (deployer) {
-  const timeERC20TokenAddress = '0x0cF22e3d8dB81CB9e606f3a7837c48c1B6fC9a27';
+  // const timeERC20TokenAddress = '0x0cF22e3d8dB81CB9e606f3a7837c48c1B6fC9a27';
+  const blockAddress = '0x6fF83eb56d9Df4007eb3593757351F2FB0011F19';
   const MIMAddress = '0xb701fCdF657CDa7256bF1ecFf340cae590Fc1286';
-  const treasury = deployer.deploy(Treasury, timeERC20TokenAddress, MIMAddress, 0, 10);
-  // const wonderZapIn = deployer.deploy(WonderZapIn, daoAddress.address);
-  // const wMEMO_ = deployer.deploy(wMEMO, memoriesERC20.address);
+  // const treasury = deployer.deploy(Treasury, timeERC20TokenAddress, MIMAddress, 0, 10);
+  const treasury = deployer.deploy(Treasury, blockAddress, MIMAddress, 0, 10);
 };
