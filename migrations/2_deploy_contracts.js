9a546551ceb4fe6b452657276dd5ef406c85d8ce
davidm <ok.star@outlook.com>
2022-01-11 07:54:46 +0800

    update staking

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,19 +1,7 @@
-// const Staking = artifacts.require("TimeStaking");
 const Staking = artifacts.require("BlockStaking");
-// const StakingHelper = artifacts.require("StakingHelper");
-// const StandardBondingCalculator = artifacts.require("StandardBondingCalculator");
-// const Treasury = artifacts.require("Treasury");
-// const WonderZapIn = artifacts.require("WonderZapIn");
-// const wMEMO = artifacts.require("wMEMO");
 
 module.exports = function (deployer) {
-  // const timeERC20TokenAddress = '0x0cF22e3d8dB81CB9e606f3a7837c48c1B6fC9a27';
   const blockAddress = '0x6fF83eb56d9Df4007eb3593757351F2FB0011F19';
-  const memoriesERC20Address = '0x27483bd899EfF7D95d0ECb14a167eFcbEe919925';
-  const stakingAddress = deployer.deploy(Staking, blockAddress, memoriesERC20Address, 28800, 440, 1641333731);
-  // const stakingHelper = deployer.deploy(StakingHelper, staking.address, timeERC20TokenAddress.address);
-  // const standardBondingCalculator = deployer.deploy(StandardBondingCalculator, timeERC20TokenAddress.address);
-  // const treasury = deployer.deploy(Treasury);
-  // const wonderZapIn = deployer.deploy(WonderZapIn, daoAddress.address);
-  // const wMEMO_ = deployer.deploy(wMEMO, memoriesERC20.address);
+  const zBlockERC20Address = '0x55e0196DDA713BD5bD34C6B986adabE011cB8138';
+  deployer.deploy(Staking, blockAddress, zBlockERC20Address, 28800, 440, 1641858767);
 };
