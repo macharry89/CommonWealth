5d4117ecb6d8dbec9e46a39427e9cab12583cbfd
davidm <ok.star@outlook.com>
2022-01-11 00:58:38 +0800

    deploy & verify block eth bond depository

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,10 @@
-const TimeBondDepository = artifacts.require("TimeBondDepository");
+const BlockBondDepository = artifacts.require("BlockBondDepository");
 
 module.exports = function (deployer) {
-  const timeAddress = '0x0cF22e3d8dB81CB9e606f3a7837c48c1B6fC9a27';
-  const anyswapV5ERC20 = '0x9D4409914B92c4B6e44CD73413e3369e7F3335B7';
-  const treasury = '0x07B834EBAA121566F89cAc43d712813c91bF8bD0';
+  const blockAddress = '0x6fF83eb56d9Df4007eb3593757351F2FB0011F19';
+  const WAVAX = '0xd00ae08403B9bbb9124bB305C09058E32C39A48c';
+  const treasury = '0x0D0817A7De44FE7685c2e35144AD8dAB5Df062fB';
   const dao = '0x90E03F137288c7dC2aD942b25Faeaa98E5b3D0C9';
   const feed = '0x8581D1D42b5Cb8d6fA2A901ebbdD0BD4f19aB9f6'; //EACAggregatorProxy
-  deployer.deploy(TimeBondDepository, timeAddress, anyswapV5ERC20, treasury, dao, feed);
+  deployer.deploy(BlockBondDepository, blockAddress, WAVAX, treasury, dao, feed);
 };
