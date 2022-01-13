74ee37cf55d127ab96135c25218938e8eaaa5a4c
davidm <ok.star@outlook.com>
2022-01-13 23:01:51 +0800

    link new zblock

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,6 @@ const Staking = artifacts.require("BlockStaking");
 
 module.exports = function (deployer) {
   const blockAddress = '0x6fF83eb56d9Df4007eb3593757351F2FB0011F19';
-  const zBlockERC20Address = '0x55e0196DDA713BD5bD34C6B986adabE011cB8138';
+  const zBlockERC20Address = '0x6C766bC8C5Cee89Ac43BD57630E3aF613e7EB89e';
   deployer.deploy(Staking, blockAddress, zBlockERC20Address, 28800, 440, 1641858767);
 };
