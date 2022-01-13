4034a335cb741cf5da3c5c2aee9ab1934bec6291
davidm <ok.star@outlook.com>
2022-01-13 22:33:50 +0800

    link new staking

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,6 @@ const StakingHelper = artifacts.require("BlockStakingHelper");
 
 module.exports = function (deployer) {
   const blockAddress = '0x6fF83eb56d9Df4007eb3593757351F2FB0011F19';
-  const stakingAddress = '0x7D71beaa180491F85CD37d87363D4a0DF8F88b20';
+  const stakingAddress = '0x52e1232D93fe13fCF38e0b4907094ff1Bc4c88ff';
   deployer.deploy(StakingHelper, stakingAddress, blockAddress);
 };
